0d6da4bac3045861c4cb70f1a6a812533d9908de
Eliyar Eziz <eliyar917@gmail.com>
2022-10-23 13:25:02 +0800

    🐛 Fix tag logic

--- Readme.md
+++ Readme.md
@@ -4,12 +4,17 @@ This is a personal tool to build python image with various base image to suit pe
 
 ## How to trigger buid
 
-1. Create a new release with tag in format `<python-version>@<base-image-tag>`, you need to replace the `:` in `base-image-tag` with `--`. For example, `3.8.6@nvidia/cuda--11.3.0-runtime-ubuntu20.04`
-2. This will trigger a github action, and build a image with tag `3.8.6---nvidia/cuda--11.3.0-runtime-ubuntu20.04`
+Replace char rules:
+
+- `\` -> `---`
+- `:` -> `--`
+
+1. Create a new release with tag in format `<python-version>@<base-image-tag>`.
+2. This will trigger a github action, and build a image with tag `3.8.6..nvidia---cuda--11.3.0-runtime-ubuntu20.04`
 
 ## Pre-build images
 
-You can get pre-build images here: [Docker hub]()
+You can get pre-build images here: [Docker hub](https://hub.docker.com/repository/docker/brikerman/python)
 
 ## Links
 
--- build-helper.sh
+++ build-helper.sh
@@ -6,12 +6,9 @@ BASE_IMAGE_SAFE_TAG=`echo $IN | cut -d \@ -f 2`
 BASE_IMAGE=$(echo "$BASE_IMAGE_SAFE_TAG" | sed "s/\-\-/\:/g")
 
 echo "Building Python $PY_VERSION on $BASE_IMAGE"
-if [[ $BASE_IMAGE = "ubuntu:20.04" ]]
-then
-  DOCKER_TAG="${PY_VERSION}"
-else
-  DOCKER_TAG="${PY_VERSION}--${BASE_IMAGE_SAFE_TAG}"
-fi
+
+DOCKER_TAG="${PY_VERSION}..${BASE_IMAGE_SAFE_TAG}"
+DOCKER_TAG=$(echo "$DOCKER_TAG" | sed "s/\//\-\-\-/g")
 
 echo "Build python image with tag: ${DOCKER_TAG}"
 echo "BASE IMAGE SAFE TAG: ${BASE_IMAGE_SAFE_TAG}"
@@ -23,4 +20,4 @@ echo "PY_VERSION=${PY_VERSION}" >> $GITHUB_ENV
 echo "BASE_IMAGE=${BASE_IMAGE}" >> $GITHUB_ENV
 
 # Use this when builing locally
-# docker build . --build-arg PY_VERSION=${PY_VERSION} --build-arg BASE_IMAGE=${BASE_IMAGE} -t ${DOCKER_TAG}
+docker build . --build-arg PY_VERSION=${PY_VERSION} --build-arg BASE_IMAGE=${BASE_IMAGE} -t "brikerman/python:${DOCKER_TAG}"
