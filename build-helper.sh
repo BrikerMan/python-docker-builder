81212d8a3007fe511a04b81483b29ecd1616d937
Eliyar Eziz <eliyar917@gmail.com>
2023-10-17 17:39:52 +0800

    Update

--- build-helper.sh
+++ build-helper.sh
@@ -12,21 +12,17 @@ DOCKER_TAG="${PY_VERSION}..${BASE_IMAGE_SAFE_TAG}"
 
 echo $USE_SHORT_PY_VERSION
 
-if [[ $USE_SHORT_PY_VERSION == "true" ]]; then
-    SHORT_PY_VERSION=$(echo "$PY_VERSION" | sed "s/\.[0-9]*$//g")
-    SHORT_TAG="${SHORT_PY_VERSION}..${BASE_IMAGE_SAFE_TAG}"
-    echo "Build python image with short tag : ${SHORT_TAG}"
-    echo "DOCKER_FULL_TAG=brikerman/python:${DOCKER_TAG},brikerman/python:${SHORT_TAG}" >> $GITHUB_ENV
-    export DOCKER_TAG=$SHORT_TAG
-else
-    echo "DOCKER_FULL_TAG=brikerman/python:${DOCKER_TAG}" >> $GITHUB_ENV
-    export DOCKER_TAG=$DOCKER_TAG
-fi
+SHORT_PY_VERSION=$(echo "$PY_VERSION" | sed "s/\.[0-9]*$//g")
+SHORT_TAG="${SHORT_PY_VERSION}..${BASE_IMAGE_SAFE_TAG}"
+
+echo "DOCKER_SHORT_TAG=${SHORT_TAG}" >> $GITHUB_ENV
+echo "DOCKER_FULL_TAG=${DOCKER_TAG}" >> $GITHUB_ENV
 
 echo "PY_VERSION=${PY_VERSION}" >> $GITHUB_ENV
 echo "BASE_IMAGE=${BASE_IMAGE}" >> $GITHUB_ENV
 
 echo "Build python image with tag: ${DOCKER_TAG}"
+echo "DOCKER_SHORT_TAG : ${SHORT_TAG}"
 echo "BASE IMAGE SAFE TAG: ${BASE_IMAGE_SAFE_TAG}"
 echo "Base image: ${BASE_IMAGE}"
 echo "Python version: ${PY_VERSION}"
